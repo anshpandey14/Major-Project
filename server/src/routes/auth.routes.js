965dc741a4538b593955925b3e0c834bd957a147
ansh pandey <anshpndey@gmail.com>
2026-06-08 23:36:48 +0530

    add auth routes

--- server/src/routes/auth.routes.js
+++ server/src/routes/auth.routes.js
@@ -3,20 +3,35 @@ import {
   registerUser,
   login,
   logoutUser,
+  refreshAccessToken,
+  getCurrentUser,
+  changePassword,
 } from "../controllers/auth.controllers.js";
 import { validate } from "../middlewares/validator.middleware.js";
 import {
   userRegisterValidator,
   userLoginValidator,
+  userChangeCurrentPasswordValidator,
 } from "../validators/index.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
+// unsecured routes
 router.route("/register").post(userRegisterValidator(), validate, registerUser);
 router.route("/login").post(userLoginValidator(), validate, login);
+router.route("/refresh-token").post(refreshAccessToken);
 
 //secure routes
 router.route("/logout").post(verifyJWT, logoutUser);
+router.route("/current-user").post(verifyJWT, getCurrentUser);
+router
+  .route("/change-password")
+  .post(
+    verifyJWT,
+    userChangeCurrentPasswordValidator(),
+    validate,
+    changePassword,
+  );
 
 export default router;
