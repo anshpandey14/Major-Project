10ff8856c5307abbc441333564080a7e54d7e5e4
ansh pandey <anshpndey@gmail.com>
2026-06-08 20:05:17 +0530

    fixed typos

--- server/src/routes/auth.routes.js
+++ server/src/routes/auth.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/auth.controllers.js";
+import { registerUser,login } from "../controllers/auth.controllers.js";
 import { validate } from "../middlewares/validator.middleware.js";
 import {
   userRegisterValidator,
@@ -8,7 +8,7 @@ import {
 
 const router = Router();
 
-router.route("/register").post(userRegisterValidator, validate, registerUser);
-router.route("/login").post(userLoginValidator, validate, registerUser);
+router.route("/register").post(userRegisterValidator(), validate, registerUser);
+router.route("/login").post(userLoginValidator(), validate, login);
 
 export default router;
