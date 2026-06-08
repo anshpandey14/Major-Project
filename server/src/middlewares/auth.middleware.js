60b8aed1d13a99e9cca272138337b033b791d0da
ansh pandey <anshpndey@gmail.com>
2026-06-08 22:41:27 +0530

    add auth middleware

---
+++ server/src/middlewares/auth.middleware.js
@@ -0,0 +1,36 @@
+import { User } from "../models/user.models.js";
+import { ApiError } from "../utils/api-error.js";
+import { asyncHandler } from "../utils/async-handler.js";
+import jwt from "jsonwebtoken";
+
+export const verifyJWT = asyncHandler(async (req, res, next) => {
+  const token =
+    req.cookies?.accessToken ||
+    req.header("Authorization")?.replace("Bearer ", "");
+
+  if (!token) {
+    throw new ApiError(401, "Unauthorized request");
+  }
+
+  const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+  const user = await User.findById(decodedToken?._id).select(
+    "-password -refreshToken",
+  );
+
+  if (!user) {
+    throw new ApiError(401, "Invalid access Token");
+  }
+  if (!user.isActive) {
+    throw new ApiError(403, "Account deactivated");
+  }
+
+  req.user = user;
+  next();
+});
+
+export const requirePHC = asyncHandler(async (req, res, next) => {
+  if (req.user.role !== "phc") {
+    throw new ApiError(403, "Only PHC staff can perform this action");
+  }
+  next();
+});
