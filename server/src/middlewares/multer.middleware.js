49229f244c6dd3182ebc6d4653ea6b46cc6a965a
anshpandey14 <anshpndey@gmail.com>
2026-06-17 18:20:28 +0530

    add cloudinary and multer setup

---
+++ server/src/middlewares/multer.middleware.js
@@ -0,0 +1,31 @@
+import multer from "multer";
+import path from "path";
+import { ApiError } from "../utils/api-error.js";
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./public/images");
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, uniqueSuffix + path.extname(file.originalname));
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  const allowedTypes = ["image/jpeg", "image/jpg", "image/png", "image/webp"];
+  if (allowedTypes.includes(file.mimetype)) {
+    cb(null, true);
+  } else {
+    cb(new ApiError(400, "Only jpeg,png and webp images are allowed"), false);
+  }
+};
+
+export const uploadAvatar = multer({
+  storage,
+  fileFilter,
+  limits: {
+    fileSize: 2 * 1024 * 1024,
+    files: 1,
+  },
+});
