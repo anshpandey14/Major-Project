fedb1130241017ea19d51b06be057a0ae5df6a14
anshpandey14 <anshpndey@gmail.com>
2026-07-19 00:04:11 +0530

    add get all visits controller

--- server/src/controllers/visit.controllers.js
+++ server/src/controllers/visit.controllers.js
@@ -3,6 +3,7 @@ import { Visit } from "../models/visit.models.js";
 import { asyncHandler } from "../utils/async-handler.js";
 import { ApiError } from "../utils/api-error.js";
 import { ApiResponse } from "../utils/api-response.js";
+import { UserRolesEnum } from "../utils/constants.js";
 
 const createVisit = asyncHandler(async (req, res) => {
   const { patientId } = req.params;
@@ -27,7 +28,7 @@ const createVisit = asyncHandler(async (req, res) => {
   }
 
   const visit = await Visit.create({
-    patient : patientId,
+    patient: patientId,
     conductedBy: req.user._id,
     visitDate,
     weight,
@@ -42,4 +43,57 @@ const createVisit = asyncHandler(async (req, res) => {
     .json(new ApiResponse(201, visit, "Visit created Successfully"));
 });
 
-export { createVisit };
+const getAllVisits = asyncHandler(async (req, res) => {
+  const { patientId } = req.params;
+
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  const patient = await Patient.findOne({
+    _id: patientId,
+    isActive: true,
+  });
+
+  if (!patient) {
+    throw new ApiError(404, "Patient not found");
+  }
+
+  if (
+    req.user.role === UserRolesEnum.ASHA &&
+    patient.assignedASHA.toString() !== req.user._id.toString()
+  ) {
+    throw new ApiError(
+      403,
+      "You are not authorized to access this patient's visits",
+    );
+  }
+
+  const [visits, totalVisits] = await Promise.all([
+    Visit.find({ patient: patientId, isActive: true })
+      .populate("conductedBy", "fullName username")
+      .sort({ visitDate: -1 })
+      .skip(skip)
+      .limit(limit),
+
+    Visit.countDocuments({ patient: patientId }),
+  ]);
+
+  return res.status(200).json(
+    new ApiResponse(
+      200,
+      {
+        visits,
+        pagination: {
+          page,
+          limit,
+          totalVisits,
+          totalPages: Math.ceil(totalVisits / limit),
+        },
+      },
+      "Visits fetched Successfully",
+    ),
+  );
+});
+
+export { createVisit, getAllVisits };
